e7972eae2f8cd3c51cd304a8ab7f39d59e41ac8e
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-03-18 11:31:27 +0500

    Fix typo

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -430,8 +430,7 @@ static int map_create(union bpf_attr *attr)
  	}
  #endif
 	
-	err = bpf_obj
-		_name_cpy(map->name, attr->map_name);
+	err = bpf_obj_name_cpy(map->name, attr->map_name);
 	if (err)
 		goto free_map_nouncharge;
 
