f419f181315401dde56609e5ab034f2b9b0c2a36
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-03-18 11:17:26 +0500

    kernel: bpf: Pretend to support ANY map_type

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -65,8 +65,11 @@ static struct bpf_map *array_map_alloc(union bpf_attr *attr)
 	if (attr->max_entries == 0 || attr->key_size != 4 ||
 	    attr->value_size == 0 ||
 	    attr->map_flags & ~ARRAY_CREATE_FLAG_MASK ||
-	    (percpu && numa_node != NUMA_NO_NODE))
-                return ERR_PTR(-EINVAL);
+	    (percpu && numa_node != NUMA_NO_NODE)
+#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
+ 		if (attr->map_type != BPF_MAP_TYPE_DUMMY)
+ #endif
+ 			return ERR_PTR(-EINVAL);
 
 	if (attr->value_size > KMALLOC_MAX_SIZE)
 		/* if value_size is bigger, the user space won't be able to
